2efacb4174f9c6b82a5f9ba6c95b51d7704ef4a9
unknown <artur.gorzkiewicz.it@gmail.com>
2023-04-24 11:14:52 +0200

    'settings'

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+for dirpath, dirnames, filenames in os.walk(PROJECT_ROOT):
+    for filename in filenames:
+        sys.path.append(os.path.join(dirpath, filename))
+
+
+print(sys.path)
